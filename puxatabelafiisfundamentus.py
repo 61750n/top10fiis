1e256cabbff625b4ffc477a7a5c18ea2ee9247b7
Gilson de Moura <140326022+61750n@users.noreply.github.com>
2023-10-31 08:17:56 -0300

    Add files via upload

---
+++ puxatabelafiisfundamentus.py
@@ -0,0 +1,93 @@
+# PROGRAMA QUE IMPORTA A TABELA DE DADOS DE FIIS DO SITE FUNDAMENTUS, TRATA E CLASSIFICA AS EMPRESAS E EXPORTA PARA EXCEL
+ 
+#1) INSTALAÇÕES INICIAIS
+# pip install pandas
+# pip install selenium
+# pip install webdriver-manager
+# pip install lxml
+
+# 2) IMPORTAÇÕES DAS BIBLIOTECAS
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+import pandas as pd
+
+# 3) ABRE NAVEGADOR --> ACESSA SITE
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+url = 'https://www.fundamentus.com.br/fii_resultado.php'
+
+driver.get(url)
+
+# 4) LÊ A TABELA DE AÇÕES --> FECHA SITE
+local_tabela = '/html/body/div[1]/div[2]/table'
+
+elemento = driver.find_element("xpath", local_tabela)
+
+html_tabela = elemento.get_attribute('outerHTML')
+
+tabela = pd.read_html(str(html_tabela), thousands = '.', decimal = ',')[0]
+
+# EXTRA: EXIBE TABELA IMPORTADA PARA VERIFICAÇÃO SE A IMPORTAÇÃO E LEITURA DERAM CERTO
+#print(tabela)
+
+#EXTRA: EXIBE O TIPO DE DADO QUE TEM NAS COLUNAS DA TABELA. INT É NÚMERO INTEIRO, FLOAT64 É NÚMERO DECIMAL, 
+#tabela.info()
+
+# 5) TRATAMENTO DO TIPO DE DADO DAS COLUNAS. DE OBJECT PARA FLOAT
+#5.1) COLUNA Dividend Yield
+tabela['Dividend Yield'] = tabela['Dividend Yield'].str.replace("%", "")
+tabela['Dividend Yield'] = tabela['Dividend Yield'].str.replace(".", "")
+tabela['Dividend Yield'] = tabela['Dividend Yield'].str.replace(",", ".")
+tabela['Dividend Yield'] = tabela['Dividend Yield'].astype(float)
+
+#5.2) COLUNA FFO Yield
+tabela['FFO Yield'] = tabela['FFO Yield'].str.replace("%", "")
+tabela['FFO Yield'] = tabela['FFO Yield'].str.replace(".", "")
+tabela['FFO Yield'] = tabela['FFO Yield'].str.replace(",", ".")
+tabela['FFO Yield'] = tabela['FFO Yield'].astype(float)
+
+#5.3) COLUNA Mrg Ebit
+tabela['Vacância Média'] = tabela['Vacância Média'].str.replace("%", "")
+tabela['Vacância Média'] = tabela['Vacância Média'].str.replace(".", "")
+tabela['Vacância Média'] = tabela['Vacância Média'].str.replace(",", ".")
+tabela['Vacância Média'] = tabela['Vacância Média'].astype(float)
+
+#5.4) COLUNA Cap Rate
+tabela['Cap Rate'] = tabela['Cap Rate'].str.replace("%", "")
+tabela['Cap Rate'] = tabela['Cap Rate'].str.replace(".", "")
+tabela['Cap Rate'] = tabela['Cap Rate'].str.replace(",", ".")
+tabela['Cap Rate'] = tabela['Cap Rate'].astype(float)
+
+#6) ELIMINANDO EMPRESAS COM LIQUIDEZ DIÁRIA ABAIXO DE R$500.000,00. 
+# ISTO SIGNIFICA QUE É RÁPIDO E FÁCIL COMPRAR E VENDER ESSAS AÇÕES
+tabela = tabela[tabela['Liquidez'] > 500000]
+
+#EXTRA: EXIBE TABELA IMPORTADA PARA VERIFICAÇÃO SE A IMPORTAÇÃO E LEITURA DERAM CERTO
+#print(tabela)
+
+#EXTRA: EXIBE O TIPO DE DADO QUE TEM NAS COLUNAS DA TABELA. INT É NÚMERO INTEIRO, FLOAT64 É NÚMERO DECIMAL, 
+#tabela.info()
+
+#7) ELIMINANDO EMPRESAS COM Vacância Média ABAIXO DE 21%
+tabela = tabela[tabela['Vacância Média'] < 20]
+
+#8) ELIMINANDO EMPRESAS COM P/VP MAIOR QUE 1
+tabela = tabela[tabela['P/VP'] < 1]
+
+#9) CLASSIFICA TABELA PELO DIVIDENDO EM ORDEM DECRESCENTE (MAIOR PARA O MENOR)
+tabela['ranking_dividendos'] = tabela['Dividend Yield'].rank(ascending = False)
+
+#10) CLASSIFICA TABELA PELO FFO Yield EM ORDEM DECRESCENTE (MAIOR PARA O MENOR)
+tabela['ranking_ffo'] = tabela['FFO Yield'].rank(ascending = False)
+
+#11) CONSTRUÇÃO DO RANKING
+tabela['ranking_total'] = tabela['ranking_dividendos'] + tabela['ranking_ffo']
+
+tabela = tabela.sort_values('ranking_total')
+
+#12) EXIBE AS 10 MELHORES AÇÕES:
+print(tabela.head(10))
+
+#13) EXPORTA PARA EXCEL
+tabela.to_excel('C:/Users/gilso/OneDrive/Programacao/FIIs e Acoes/top_10_fiis.xlsx', index=False)
---
+++ readme.md
@@ -0,0 +1,55 @@
+# TOP 10 FIIS
+
+Este arquivo README fornece uma visão geral do projeto.
+
+## O que o projeto faz?
+
+O projeto TOP 10 FIIS faz um ranking das 10 melhores FIIs usando alguns elementos da análise fundamentalista:
+1- Fundos com Liquidez diária superior a R$500.000,00;
+2- Fundos com Vacância Média inferior a 20%;
+3- Fundos com P/VP menor que 1;
+A lista está classificada dessa forma:
+1- Por Dividend Yield;
+2- Por FFO Yield;
+A lista gerada é exportada como arquivo Excel (xlsx)
+
+
+## Por que o projeto é útil?
+
+O projeto é útil porque lista fundos com um arcabouço interessante para investimento, apesar de não tratar de recomendação de investimento.
+
+## Como os usuários podem começar com o projeto?
+
+Para começar com o projeto, siga estas etapas:
+
+1. Instalação do Python
+2. Instalação das bibliotecas pandas, selenium, webdriver-manager e lxml
+3. Acesso à Internet
+
+## Onde os usuários podem obter ajuda com o projeto?
+
+Os usuários podem obter ajuda com o projeto nos seguintes locais:
+
+* gilson.moura@gmail.com
+
+## Quem mantém e contribui para o projeto?
+
+O projeto é mantido por Gilson de Moura.
+
+## Licença
+
+O projeto é licenciado sob a GNU GPL v3.
+
+## Contribuições
+
+Contribuições são bem-vindas! Para contribuir com o projeto, siga estas etapas:
+
+1. Faça um fork do repositório.
+2. Crie uma branch para sua contribuição.
+3. Faça suas alterações.
+4. Faça um commit de suas alterações.
+5. Envie um pull request para a branch principal.
+
+## Versão
+
+Esta é a versão 1.0
